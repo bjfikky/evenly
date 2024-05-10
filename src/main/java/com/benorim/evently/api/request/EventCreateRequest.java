cb2c355c7f6e4dd62c593044c3193b4fe72039a9
Benjamin Orimoloye <bjfikky@yahoo.com>
2024-05-09 23:03:54 -0400

    Added Event API request and responses

---
+++ src/main/java/com/benorim/evently/api/request/EventCreateRequest.java
@@ -0,0 +1,20 @@
+package com.benorim.evently.api.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record EventCreateRequest(
+        @NotBlank String title,
+        String description,
+        String addressLine1,
+        String addressLine2,
+        String city,
+        String state,
+        String zipCode,
+        String imageUrl,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        String additionalNotes
+        ) {
+}
---
+++ src/main/java/com/benorim/evently/api/response/EventResponse.java
@@ -0,0 +1,21 @@
+package com.benorim.evently.api.response;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+    Long id,
+    String title,
+    String description,
+    String addressLine1,
+    String addressLine2,
+    String city,
+    String state,
+    String zipCode,
+    String imageUrl,
+    LocalDateTime startTime,
+    LocalDateTime endTime,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    String additionalNotes
+) {
+}
