d2e3f539d7759bbf3fba268d5c0d343ad9318a27
Benjamin Orimoloye <bjfikky@yahoo.com>
2024-05-17 23:12:30 -0400

    Added ApplicationExceptionHandler and EventUpdateOrCreateException

---
+++ src/main/java/com/benorim/evently/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.benorim.evently.exception;
+
+import com.benorim.evently.api.response.ErrorResponse;
+import com.benorim.evently.enums.ErrorState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(EventUpdateOrCreateException.class)
+    @ResponseStatus(value= HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleEventCreateOrUpdateException(EventUpdateOrCreateException exception) {
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), ErrorState.CREATE_OR_UPDATE_FAILURE), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/benorim/evently/exception/EventUpdateOrCreateException.java
@@ -0,0 +1,7 @@
+package com.benorim.evently.exception;
+
+public class EventUpdateOrCreateException extends RuntimeException {
+    public EventUpdateOrCreateException(String message) {
+        super(message);
+    }
+}
