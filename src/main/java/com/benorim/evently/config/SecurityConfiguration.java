80c647b782ef7b0a3fb8ff7ef30da1097effe645
Benjamin Orimoloye <bjfikky@yahoo.com>
2024-05-18 21:05:40 -0400

    Added SecurityConfiguration

---
+++ src/main/java/com/benorim/evently/config/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.benorim.evently.config;
+
+import com.benorim.evently.enums.Role;
+import com.benorim.evently.filter.JwtAuthenticationFilter;
+import com.benorim.evently.service.EventlyUserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    public static final String AUTH_API_MATCHER = "/api/v1/auth/**";
+    public static final String VERSION_API = "/api/version";
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final EventlyUserService EventlyUserService;
+
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, EventlyUserService EventlyUserService) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.EventlyUserService = EventlyUserService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.GET, "/api/v1/events", "/api/v1/events/**").permitAll()
+                        .requestMatchers(AUTH_API_MATCHER, VERSION_API)
+                        .permitAll()
+                        .requestMatchers("/api/**").hasAnyAuthority(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(EventlyUserService.userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
