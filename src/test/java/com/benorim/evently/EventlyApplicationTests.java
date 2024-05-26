68d910aa17ff349ff90d6896d13e5b74de8b1ef0
Benjamin Orimoloye <bjfikky@yahoo.com>
2024-05-26 09:13:09 -0400

    Commented out contextLoads test

--- src/test/java/com/benorim/evently/EventlyApplicationTests.java
+++ src/test/java/com/benorim/evently/EventlyApplicationTests.java
@@ -1,15 +1,13 @@
 package com.benorim.evently;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventlyApplicationTests {
 
-    @Test
-    @Disabled
-    void contextLoads() {
-    }
+//    @Test
+//    @Disabled
+//    void contextLoads() {
+//    }
 
 }
